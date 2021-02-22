e09397023c6babb3a7eca2f3ed91faa5f37267a1
Omkar <omjadhav2610@gmail.com>
2021-02-22 23:18:13 +0530

    Remove dead code and unnecessary permission(s)

--- content.js
+++ content.js
@@ -1,19 +1,16 @@
-document.addEventListener('click',(event)=>{
-    const el = event.target;
+document.addEventListener('click', (event) => {
+	const el = event.target;
 
-    //if event.target.tagName is a the clcked element is an anchor
-    //if event.target.closest('a') is non-null value then clicked element is child of 
-    
-    const ancestorAnchor = el.closest('a');
+	//if el.tagName is 'A' the clicked element is an anchor
+	//if el.closest('a') is non-null value then clicked element is child of anchor or anchor itself
 
-    if(el.tagName=='A' || ancestorAnchor){
-        console.log("Clicked on a link!!", el.tagName,ancestorAnchor)
+	const ancestorAnchor = el.closest('a');
 
-        if(event.altKey && event.ctrlKey){
-            event.preventDefault()
-            event.stopPropagation();
-            chrome.runtime.sendMessage({url: el.href || ancestorAnchor.href})
-        }
-    }
-
-})
+	if (el.tagName == 'A' || ancestorAnchor) {
+		if (event.altKey && event.ctrlKey) {
+			event.preventDefault();
+			event.stopPropagation();
+			chrome.runtime.sendMessage(el.href || ancestorAnchor.href);
+		}
+	}
+});
